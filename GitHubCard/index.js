cf6ee201f12e0d7b69c2156219daaf2b4a214598
Jie Zhang <zhangjiew@gmail.com>
2021-07-08 16:07:14 -0600

    finally done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -27,9 +29,64 @@
     Using that array, iterate over it, requesting data for each user, creating a new card for each
     user, and adding that card to the DOM.
 */
+const makeCard = function (user) {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then((res) => {
+      //set vaule
+      const data = res.data;
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const img = document.createElement("img");
+      img.classList.add("img");
+      img.src = data.avatar_url;
+      const cardInfo = document.createElement("div");
+      cardInfo.classList.add("card-info");
+      const name = document.createElement("h3");
+      name.classList.add("name");
+      name.textContent = data.name;
+      const userName = document.createElement("p");
+      userName.classList.add("username");
+      userName.textContent = data.login;
+      const location = document.createElement("p");
+      location.textContent = `Location: ${data.location}`;
+      const profile = document.createElement("p");
+      profile.textContent = `Profile: ${data.html_url}`;
+      const followers = document.createElement("p");
+      followers.textContent = `Followers: ${data.followers}`;
+      const following = document.createElement("p");
+      following.textContent = `Following: ${data.following}`;
+      const bio = document.createElement("p");
+      bio.textContent = `Bio: ${data.bio}`;
+      //append info
+      card.append(img);
+      cardInfo.append(name);
+      cardInfo.append(userName);
+      cardInfo.append(location);
+      cardInfo.append(profile);
+      cardInfo.append(followers);
+      cardInfo.append(following);
+      cardInfo.append(bio);
+      card.append(cardInfo);
+      console.log(card);
+      //append cards
+      const cardsContainer = document.querySelector(".cards");
+      cardsContainer.appendChild(card);
+    })
+    .catch((err) => console.log(err.message, `wrong lllll`));
+};
+makeCard("jie-chelchel");
+const followersArray = [
+  "rkshockey",
+  "D2MBingo5",
+  "Klove-A",
+  "SelfCheckout94",
+  "fernando817mm",
+];
 
-const followersArray = [];
-
+followersArray.forEach((user) => {
+  makeCard(user);
+});
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
